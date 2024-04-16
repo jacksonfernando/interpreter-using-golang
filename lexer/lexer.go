48dd02c45ba6fe76e18b150a5cda76ffdd8266ce
jacksonfernando <jacksonfernando51@gmail.com>
2024-04-16 21:31:20 +0700

    add skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -21,6 +21,7 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhiteSpace()
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -44,6 +45,7 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -58,6 +60,12 @@ func (l *Lexer) readIdentifier() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func isLetter(ch byte) bool {
