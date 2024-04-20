c0e42cea9ded82ea7005a2e7797c8c54845cc523
jacksonfernando <jacksonfernando51@gmail.com>
2024-04-20 22:55:44 +0700

    add parser file

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"github.com/v2/golang-intrepeter/ast"
+	"github.com/v2/golang-intrepeter/lexer"
+	"github.com/v2/golang-intrepeter/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
