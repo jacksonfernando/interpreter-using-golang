20ac15da75fef56f9e240ae6a82911cd53d08871
jacksonfernando <jacksonfernando51@gmail.com>
2024-04-21 23:21:30 +0700

    add test let statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,64 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/v2/golang-intrepeter/ast"
+	"github.com/v2/golang-intrepeter/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() return nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"x"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+		return
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLIteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatemnt. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s', got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
+}
