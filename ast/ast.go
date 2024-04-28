79d01c2584550a3e07d016746d9865ae61d802b7
jacksonfernando <jacksonfernando51@gmail.com>
2024-04-28 23:04:13 +0700

    add statement node and token literal method of return statement

--- ast/ast.go
+++ ast/ast.go
@@ -37,6 +37,10 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
