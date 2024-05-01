0ab222b5a562d9e2249add085283e925376a5ed5
jacksonfernando <jacksonfernando51@gmail.com>
2024-05-01 21:29:58 +0700

    add ls string method

--- ast/ast.go
+++ ast/ast.go
@@ -44,6 +44,20 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (ls *LetStatement) statementNode() {}
 
 // Return Statement
