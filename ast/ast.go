a6e89049dc227d24042c9cd50383cd4ba041695e
jacksonfernando <jacksonfernando51@gmail.com>
2024-05-10 22:54:52 +0700

    add prefix expression

--- ast/ast.go
+++ ast/ast.go
@@ -103,6 +103,7 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+// Integer Literal Expression
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
@@ -113,6 +114,26 @@ func (il *IntegerLiteral) expressionNode() {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
+// Prefix Expression
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
 
 // Identifier
 type Identifier struct {
