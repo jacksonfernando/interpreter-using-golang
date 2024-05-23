7f057550fef3ae37967c309aac52af06593d93a4
jacksonfernando <jacksonfernando51@gmail.com>
2024-05-23 09:04:56 +0700

    add test literal expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,7 +18,24 @@ func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
 		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
 		return false
 	}
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TOkenLiteral not %s got=%s", value, ident.TokenLiteral())
+		return false
+	}
 	return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifier(t, exp, v)
+	}
+	t.Errorf("type of exp not handled. got=%T", exp)
+	return false
 }
 
 func TestIntegerLiteralExpression(t *testing.T) {
