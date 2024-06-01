4548f726da48700bf166d40910b6d412123abda5
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-01 09:25:56 +0700

    add boolean eval

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/v2/golang-intrepeter/evaluator"
 	"github.com/v2/golang-intrepeter/lexer"
 	"github.com/v2/golang-intrepeter/parser"
 )
@@ -28,8 +29,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
