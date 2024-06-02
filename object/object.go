cc9d01f15de52595da8e8450a8779c0e1b1b4c88
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-02 23:52:35 +0700

    add error handling in eval program

--- object/object.go
+++ object/object.go
@@ -7,6 +7,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type ObjectType string
@@ -15,6 +16,14 @@ type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+// Error
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
 // Return
 type ReturnValue struct {
