768cd1d3cba37a8e3d4a1c8390f2b78b6f8ce7d1
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-05 08:33:13 +0700

    add string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,20 @@ import (
 	"github.com/v2/golang-intrepeter/parser"
 )
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not STring. got=%T (+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
 
@@ -106,6 +120,10 @@ func TestErrorHandling(t *testing.T) {
 		{
 			"foobar",
 			"identifier not found: foobar",
+		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 
