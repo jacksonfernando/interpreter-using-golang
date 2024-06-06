b31e6abe81fb443512a498e8b97bfcceb30e06d3
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-06 21:37:29 +0700

    add index expression

--- ast/ast.go
+++ ast/ast.go
@@ -41,6 +41,27 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+// Index Expression
+type IndexExpression struct {
+	Token token.Token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
 // Array literal
 type ArrayLiteral struct {
 	Token    token.Token
