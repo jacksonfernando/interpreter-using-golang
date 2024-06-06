c5f330ab38602a8de849d87cc2588291d70d39ca
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-06 21:37:35 +0700

    add testing parser index expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,6 +8,29 @@ import (
 	"github.com/v2/golang-intrepeter/lexer"
 )
 
+func TestParsingIndexExprressions(t *testing.T) {
+	input := "myArray[1 + 1]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
+
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
+
 func TestParsingArrayLiterals(t *testing.T) {
 	input := "[1, 2 * 2, 3 + 3]"
 
