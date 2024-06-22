f4d8504ee03b709b390d3865ba1f546401e98e5c
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-22 17:28:52 +0700

    add integer evaluator

--- ast/ast.go
+++ ast/ast.go
@@ -36,6 +36,14 @@ func (p *Program) String() string {
 	return out.String()
 }
 
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
