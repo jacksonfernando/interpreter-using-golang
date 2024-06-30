c5a87e79ce73a14750f115c4eb2de24ee9dbf154
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-30 23:43:12 +0700

    asdd function evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -34,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program env)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
