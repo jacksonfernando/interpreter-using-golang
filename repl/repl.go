06695c78b8146d2d88a95117fd8ec835ed2c0907
jacksonfernando <jacksonfernando51@gmail.com>
2024-06-30 15:08:06 +0700

    add environment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/v2/golang-intrepeter/evaluator"
 	"github.com/v2/golang-intrepeter/lexer"
+	"github.com/v2/golang-intrepeter/object"
 	"github.com/v2/golang-intrepeter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
